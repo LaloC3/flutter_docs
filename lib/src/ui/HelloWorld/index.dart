f78e5ff1f915c936da212874b102f362a5baab70
Eduardo Contreras <lalo_grungr@hotmail.com>
2019-03-05 22:45:39 -0600

    update

--- lib/src/ui/HelloWorld/index.dart
+++ lib/src/ui/HelloWorld/index.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-
+// ? Inicio de la App
 class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "Hello, World",
-      textDirection: TextDirection.ltr,
+    return Center(
+      child: Text(
+        "Hello, World",
+        textDirection: TextDirection.ltr,
+      ),
     );
   }
 }
+// ? Final de la App
